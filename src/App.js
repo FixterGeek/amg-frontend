ccda3d71b9bffe99035706ff648f6206bf32f1bf
= <jo123.garcia.s@gmail.com>
2019-06-18 22:56:59 -0500

    react-router-dom is added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './styles/App.scss';
 
-import TextField from './molecules/TextFields';
+import AppRouter from './routes/Router';
 
 function App() {
-  const [state, setState] = useState(null);
-
-  const handleChange = (e) => {
-    const { value } = e.target;
-    setState(value);
-  };
-
   return (
     <div>
-      <TextField
-        onChange={handleChange}
-        value={state}
-      />
+      <AppRouter />
     </div>
   );
 }
---
+++ src/routes/Router.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Home from '../pages/Home';
+
+function AppRouter() {
+  return (
+    <Router>
+      <Route path="/" exact component={Home} />
+    </Router>
+  );
+}
+
+export default AppRouter;
