811159b9af9c932a872fea9838f190650258facd
awce <awce5nk@hotmail.com>
2019-07-08 17:27:31 -0500

    signup flow

---
+++ src/pages/NotFound.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div>
+        <h1>404!</h1>
+        <h2>No existe esta página</h2>
+      </div>
+    );
+  }
+}
+
+export default NotFound;
--- src/routes/Router.js
+++ src/routes/Router.js
@@ -5,9 +5,12 @@ import PrivateRoute from "./PrivateRoute";
 import WithoutLoginRoute from "./WithoutLoginRoute";
 import Home from "../components/home/HomeContainer";
 import Login from "../components/login/Login";
-import Registration from "../components/profile/BeforeStaring";
+import Registration from "../components/signup/BeforeStaring";
+import GeneralData from "../components/signup/GeneralData";
+import FiscalData from "../components/signup/FiscalData";
 import Signup from "../components/signup/Signup";
 import DashBoard from "../pages/DashBoard";
+import NotFound from "../pages/NotFound";
 
 function AppRouter() {
   return (
@@ -16,7 +19,10 @@ function AppRouter() {
       <WithoutLoginRoute path="/login" exact component={Login} />
       <Route path="/signup" exact component={Signup} />
       <Route path="/registration" exact component={Registration} />
+      <Route path="/registration/general" exact component={GeneralData} />
+      <Route path="/registration/fiscal" exact component={FiscalData} />
       <PrivateRoute path="/dashboard" component={DashBoard} />
+      <Route component={NotFound} />
     </Router>
   );
 }
