d9e5993ac609826d49dfb33eb0b9bd756842970e
= <jo123.garcia.s@gmail.com>
2019-07-26 20:38:13 -0500

    routes for events details

--- src/routes/DashBoardRouter.js
+++ src/routes/DashBoardRouter.js
@@ -4,13 +4,19 @@ import { Switch, Route } from 'react-router-dom';
 import Feed from '../components/feed/Feed';
 import EventsList from '../components/events/EventsList';
 import EventDetail from '../components/events/EventDetail';
+import Program from '../components/events/Program';
+import Speakers from '../components/events/Speakers';
+import ActivityDetail from '../components/events/ActivityDetail';
 
 function DashBoardRouter() {
   return (
     <Switch>
+      <Route path="/dashboard/events/:id/program/:id" component={ActivityDetail} />
+      <Route path="/dashboard/events/:id/program" component={Program} />
+      <Route path="/dashboard/events/:id/speakers" component={Speakers} />
       <Route path="/dashboard/events/:id" component={EventDetail} />
-      <Route path="/dashboard/events" component={EventsList} />
-      <Route path="/dashboard" component={Feed} />
+      <Route exact path="/dashboard/events" component={EventsList} />
+      <Route exact path="/dashboard" component={Feed} />
     </Switch>
   );
 }
