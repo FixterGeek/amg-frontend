d4e076e4ef7fd5b419d7d344a1d17153d79c8457
Bliss <Bliss@MacBook-Pro-de-Bliss.local>
2019-08-08 15:24:08 -0500

    redux configured like ducks

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,9 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './rootReducer';
+import thunk from 'redux-thunk'
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
