38b6b0edf0dd9ff42cb111c480d268182bfbf1fc
Bliss <Bliss@MacBook-Pro-de-Bliss.local>
2019-08-10 23:40:31 -0500

    list and modal component added

--- src/components/admin/AdminEventForm.js
+++ src/components/admin/AdminEventForm.js
@@ -2,7 +2,6 @@ import React, { useState, useReducer, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import TextField from '../../molecules/TextFields'
 import {
-    DatePicker,
     TimePicker,
     Select,
     Icon,
@@ -10,24 +9,30 @@ import {
 import Upload from './reusables/Upload'
 import estados from './estados.json'
 import moment from 'moment'
+import SpeakerModal from './reusables/SpeakerModal';
+import DatePicker from '../../molecules/DatePicker'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faTrash,
+} from '@fortawesome/free-solid-svg-icons';
+import { connect } from 'react-redux'
+import { saveDraftEvent } from '../../store/ducks/adminDuck'
+import ListAndModal from './reusables/ListAndModal'
 
 let { Option } = Select
 const { Dragger } = Upload;
 
-export default function AdminEventForm() {
+function AdminEventForm({
+    saveDraftEvent
+}) {
 
     let [imageUrl, setImageUrl] = useState(null)
     let [loading, setLoading] = useState(false)
+    let [speakerModal, setSpeakerModal] = useState(true)
     let [state, setState] = useReducer((state, update) => ({ ...state, ...update }), {
         mainImages: "file",
         permisos: "file",
-        speakers: [{
-            title: '',
-            fullName: '',
-            photoURL: '',
-            city: '',
-            bio: ''
-        }],
+        speakers: [],
         modules: ["ids de los modulos"],
         location: {
             addressName: "",
@@ -37,7 +42,7 @@ export default function AdminEventForm() {
             colony: "",
             zipCode: "",
             city: "",
-            state: "",
+            state: null,
             coordinates: []
         },
         title: 'titulo',
@@ -45,6 +50,8 @@ export default function AdminEventForm() {
         startTime: '',
         endDate: '',
         description: '',
+        mainImagesURLS: ["https://miro.medium.com/fit/c/256/256/0*jp3IFb08Sy3_k3N_."],
+        permisosURLS: []
 
     })
 
@@ -53,8 +60,6 @@ export default function AdminEventForm() {
     }, [state])
 
     function handleChange(e, sub) {
-        console.log(e)
-        return
         let { name, value } = e.target
         if (sub) {
             let object = state[sub]
@@ -70,104 +75,236 @@ export default function AdminEventForm() {
         setState({ [name]: date })
     }
 
+    function handleSelect(value, name, sub) {
+        if (sub) {
+            let object = state[sub]
+            object[name] = value
+            setState({ [sub]: object })
+            return
+        }
+        setState({ [name]: value })
+    }
+
+    function handleFile(file, name) {
+        setState({ [name]: file })
+    }
+
+    // function addSpeaker(newSpeaker) {
+    //     let { speakers } = state
+    //     speakers.unshift(newSpeaker)
+    //     setState({ speakers })
+    // }
+
+    // function removeSpeaker(speaker) {
+    //     let { speakers } = state
+    //     speakers = speakers.filter(s => s.fullName !== speaker.fullName)
+    //     setState({ speakers })
+    // }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        //transform to formData
+        let form = new FormData()
+        let r = transformToFormData(form, state)
+        let pics = getPicturesFilesWithOrder(state.speakers)
+        console.log("fotos speakers", pics)
+        saveDraftEvent(r)
+    }
+
+    function getPicturesFilesWithOrder(array) {
+        let result = []
+        for (let el of array) {
+            result = [...result, el.photoFile]
+        }
+        return result
+    }
+
+    function transformToFormData(formData, obj, parentKey) {
+        if (parentKey) {
+            for (let k in obj) {
+                formData.append(`${parentKey}[${k}]`, obj[k])
+            }
+        }
+        else {
+            for (let k in obj) {
+                // if (k === "permisos") {
+                //     formData.append(k, obj[k])
+                //     continue
+                // }
+                if (Array.isArray(obj[k]) || typeof obj[k] === "object") {
+                    formData.append(k, JSON.stringify(obj[k]))
+                    continue
+                }
+                // if (typeof obj[k] === "object") transformToFormData(formData, obj[k], k)
+                else formData.append(k, obj[k])
+            }
+        }
+        return formData
+    }
+
     return (
         <div className="admin-event-form-container">
-            <h1>Nuevo Evento</h1>
-            <form
-                className="admin-flex-column"
-                onSubmit={() => { }}>
+            <div className="admin-form-header">
+                <h1>Nuevo Evento</h1>
+                <button>Guardar como borrador</button>
+            </div>
+            <div className="admin-form-two-columns-container">
 
-                <TextField
-                    value={state.title}
-                    onChange={handleChange}
-                    marginTop={10}
-                    name="title"
-                    label="Nombre del evento"
-                    placeholder="Evento interdiciplinario"
-                />
-                <TextField
-                    onChange={handleChange}
-                    name="description"
-                    value={state.description}
-                    style={{ width: 300 }}
-                    label="Descripción del evento"
-                    placeholder="describe el evento"
-                    type="textarea"
-                />
-                <b>Fechas:</b>
-                <br />
-                <div className="admin-form-group">
-                    <b>De:</b>
+
+                <form
+                    className="admin-flex-column"
+                    onSubmit={handleSubmit}>
+
+                    <TextField
+                        value={state.title}
+                        onChange={handleChange}
+                        marginTop={10}
+                        name="title"
+                        label="Nombre del evento"
+                        placeholder="Evento interdiciplinario"
+                    />
+                    <TextField
+                        onChange={handleChange}
+                        name="description"
+                        value={state.description}
+                        style={{ width: 300 }}
+                        label="Descripción del evento"
+                        placeholder="describe el evento"
+                        type="textarea"
+                    />
+                    <b>Fechas:</b>
+                    <br />
                     <DatePicker
+                        label="De:"
                         onChange={m => handleDate(m, "startDate")}
                         style={{ width: 300 }}
                         format="LL"
                         placeholder="Selecciona la fecha"
                         value={state.startDate ? moment(state.startDate) : null}
                     />
-                </div>
-                <div className="admin-form-group">
-                    <b>A:</b>
                     <DatePicker
+                        label="A:"
                         onChange={m => handleDate(m, "endDate")}
                         style={{ width: 300 }}
                         format="LL"
                         placeholder="Selecciona la fecha"
                         value={state.endDate ? moment(state.endDate) : null}
                     />
-                </div>
-                <div className="admin-form-group">
-                    <b>Hora de  inicio</b>
-                    <TimePicker
-                        onChange={m => handleDate(m, "startTime")}
-                        placeholder="Selecciona la hora"
+                    <div className="admin-form-group">
+                        <b>Hora de  inicio</b>
+                        <TimePicker
+                            onChange={m => handleDate(m, "startTime")}
+                            placeholder="Selecciona la hora"
+                            style={{ width: 300 }}
+                            value={state.startTime ? moment(state.startTime) : null}
+                        />
+                    </div>
+                    <TextField
+                        onChange={e => handleChange(e, "location")}
+                        name="street"
+                        value={state.location.street}
                         style={{ width: 300 }}
-                        value={state.startTime ? moment(state.startTime) : null}
+                        label="Dirección"
+                        placeholder="Calle"
                     />
-                </div>
-                <TextField
-                    style={{ width: 300 }}
-                    name="street"
-                    label="Dirección"
-                    placeholder="Calle"
-                />
-                <TextField
-                    style={{ width: 300 }}
-                    name="zipCode"
-                    label="Código postal"
-                    placeholder="07239"
-                />
-                <TextField
-                    name="city"
-                    label="Ciudad"
-                    placeholder="San Cristobal"
-                    style={{ width: 300 }}
-                />
-                <div className="admin-form-group">
-                    <b>Estado</b>
-                    <Select
+                    <TextField
+                        onChange={e => handleChange(e, "location")}
+                        name="zipCode"
+                        value={state.location.zipCode}
                         style={{ width: 300 }}
-                        defaultValue="Ciudad de México">
-                        {Object.values(estados).map(name => {
-                            return <Option value={name} >{name}</Option>
-                        })}
-                    </Select>
-                </div>
-                <div className="admin-form-group">
-                    <b>Imagen del evento</b>
-                    <Upload />
-                </div>
-                <div className="admin-form-group">
-                    <b>Carta permiso</b>
-                    <Upload accept="application/pdf" />
-                </div>
-                <input
-                    className="admin-form-submit-button"
-                    type="submit" value="Publicar evento" />
+                        label="Código postal"
+                        placeholder="07239"
+                    />
+                    <TextField
+                        onChange={e => handleChange(e, "location")}
+                        name="city"
+                        value={state.location.city}
+                        label="Ciudad"
+                        placeholder="San Cristobal"
+                        style={{ width: 300 }}
+                    />
+                    <div className="admin-form-group">
+                        <b>Estado</b>
+                        <Select
+                            onChange={e => handleSelect(e, "state", "location")}
+                            value={state.location.state}
+                            style={{ width: 300 }}
+                            defaultValue="Ciudad de México">
+                            {Object.values(estados).map(name => {
+                                return <Option value={name} >{name}</Option>
+                            })}
+                        </Select>
+                    </div>
+                    <div className="admin-form-group">
+                        <b>Imagen del evento</b>
+                        <Upload
+                            preview={state.mainImagesURLS[0]}
+                            onChange={file => handleFile(file, "mainImages")}
+                        />
+                    </div>
+                    <div className="admin-form-group">
+                        <b>Carta permiso</b>
+                        <Upload
+                            preview={state.permisosURLS[0]}
+                            onChange={file => handleFile(file, "permisos")}
+                            accept="application/pdf" />
+                    </div>
+                    {/* <div className="admin-form-group">
+                        <b>Ponentes</b>
+
+                        {state.speakers.map(s => (
+                            <div className="admin-form-speaker" >
+                                {s.title} {s.fullName}
+                                <Icon
+                                    onClick={() => removeSpeaker(s)}
+                                >
+                                    <FontAwesomeIcon icon={faTrash} />
+                                </Icon>
+                            </div>)
+                        )}
+
+                        <div
+                            onClick={() => { setSpeakerModal(true) }}
+                            style={{ textAlign: "center" }}
+                            className="admin-form-button-add">
+                            Agregar ponente
+                        </div>
+                    </div> */}
+
+                    <ListAndModal
+                        modal={< SpeakerModal />}
+                        label="Ponentes"
+                        buttonText="Agregar Ponente"
+                        keys={["title", "fullName"]}
+                        onChange={list => handleChange({ target: { name: "speakers", value: list } })}
+                    />
+
+                    <input
+                        className="admin-form-submit-button"
+                        type="submit" value="Publicar evento" />
 
 
 
-            </form>
+                </form>
+
+
+
+            </div>
+
+            {/* modals */}
+            {/* {speakerModal && <SpeakerModal
+                onFinish={addSpeaker}
+                onCancel={() => setSpeakerModal(false)}
+            />} */}
         </div>
     )
 }
+
+function mapState({ admin }) {
+    let events = Object.values(admin.draftEvents)
+    return {
+        events
+    }
+}
+
+export default connect(mapState, { saveDraftEvent })(AdminEventForm)
--- src/store/store.js
+++ src/store/store.js
@@ -10,6 +10,8 @@ import publicationsDuck from './ducks/publicationsDuck';
 import userDuck from './ducks/userDuck';
 import usersReducer from './ducks/users';
 import sigupDuck from './ducks/signupDuck';
+import configReducer from './ducks/configDuck'
+import adminReducer, { saveDraftEventEpic } from './ducks/adminDuck'
 
 let rootReducer = combineReducers({
     user: userDuck,
@@ -17,12 +19,21 @@ let rootReducer = combineReducers({
     publications: publicationsDuck,
     users: usersReducer,
     signup: sigupDuck,
+    config: configReducer,
+    admin: adminReducer
 });
 
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const rootEpic = combineEpics(() => of({ type: "EPIC" }), loginUserEpic, persistEpic, hydrateEpic, getUsersEpic)
+const rootEpic = combineEpics(() => of({ type: "EPIC" }),
+    loginUserEpic,
+    persistEpic,
+    hydrateEpic,
+    getUsersEpic,
+    saveDraftEventEpic,
+
+)
 
 const epicMiddleware = createEpicMiddleware();
 
