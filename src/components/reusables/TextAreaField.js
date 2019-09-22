791c69cd9309794ac2ad50e80bab6c180f39124e
Jorge Garcia <georgge@MacBook-Pro-de-Jorge.local>
2019-09-21 23:11:10 -0500

    text area like field

---
+++ src/components/reusables/TextAreaField.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Form, Input } from 'antd';
+
+function TextAreaField({
+  label, onChange, value,
+  placeholder, styles, name,
+  rows
+}) {
+  const { Item } = Form;
+  const { TextArea } = Input;
+
+  const handleChange = (event) => {
+    if (onChange) onChange(event);
+  };
+
+  return (
+    <Item label={label} className="reusables-text-area-field">
+      <TextArea
+        onChange={handleChange}
+        value={value}
+        placeholder={placeholder}
+        name={name}
+        styles={styles}
+        rows={rows}
+      />
+    </Item>
+  );
+}
+
+export default TextAreaField;
+
+TextAreaField.propTypes = {
+  label: PropTypes.string,
+  onChange: PropTypes.func,
+  value: PropTypes.string,
+  placeholder: PropTypes.string,
+  styles: PropTypes.object,
+  name: PropTypes.string,
+  rows: PropTypes.number,
+}
+
+TextAreaField.defaultProps = {
+  label: null,
+  onChange: null,
+  value: null,
+  placeholder: null,
+  styles: null,
+  name: null,
+  rows: 4,
+}
