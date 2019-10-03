6619fca7dbb8d2cbe789715a15b1cb90a3a2001d
Jorge Garcia <georgge@MacBook-Pro-de-Jorge.local>
2019-10-02 21:36:31 -0500

    testing capcha

--- src/components/home/reusables/HomeContactForm.js
+++ src/components/home/reusables/HomeContactForm.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { Form } from 'antd';
 import TextField from '../../reusables/TextField';
 import SelectField, { OptionSelect } from '../../reusables/SelectField';
+import Button from '../../reusables/Button';
 
 function HomeContactForm() {
   const [contactData, setContactData] = useState({
@@ -16,8 +17,13 @@ function HomeContactForm() {
     setContactData({ ...contactData, [name]: value });
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(event.target);
+  };
+
   return (
-    <Form>
+    <div className="home-reusables-contact-form">
       <TextField
         onChange={handleChange}
         name="email"
@@ -36,7 +42,15 @@ function HomeContactForm() {
         <OptionSelect value="Revista">Revista</OptionSelect>
         <OptionSelect value="Técnico">Técnico</OptionSelect>
       </SelectField>
-    </Form>
+      <form action="?" method="POST" onSubmit={handleSubmit}>
+        <div class="g-recaptcha" data-sitekey="6LfXl7sUAAAAANAztRRuFtdE19H_FSlduja29Qg-" />
+          <br/>
+        <input type="submit" value="Verificar" />
+      </form>
+      <Button width="100%">
+        Enviar
+      </Button>
+    </div>
   );
 }
 
