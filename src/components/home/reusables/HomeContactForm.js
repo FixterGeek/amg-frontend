e29eb9440b5d42d8cf122be11be1cacb6f767361
Brenda Ortega <Brenda@Brendas-MacBook-Pro.local>
2019-11-23 17:42:34 -0600

    contacto

--- src/components/home/reusables/HomeContactForm.js
+++ src/components/home/reusables/HomeContactForm.js
@@ -1,90 +1,91 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, {Component, useState} from 'react';
+import NavBar from "../../organisms/NavBar";
+import TextField from "../../molecules/TextFields";
+import {Select} from "antd";
+import { message} from 'antd';
+import InputField from "../../atoms/data_entry/InputField";
 
-import { message } from 'antd';
+const baseURL = process.env.REACT_APP_BASE_API_URL;
 
-import useSweet from '../../../hooks/useSweetAlert';
-import TextField from '../../reusables/TextField';
-import SelectField, { OptionSelect } from '../../reusables/SelectField';
-import Button from '../../reusables/Button';
+const { Option } = Select;
 
-function HomeContactForm() {
-  const {  infoAlert} = useSweet();
-  const [disabledSendButton, setDisabledSendButton] = useState(true);
-  const [contactData, setContactData] = useState({
-    email: null,
-    name: null,
-    topic: null,
-  });
 
-  const handleChange = ({ target }) => {
-    const { name, value } = target;
-    setContactData({ ...contactData, [name]: value });
-  };
+class HomeContactForm extends Component {
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const form = event.target;
-    const textArea = form.querySelectorAll('div')[0].querySelectorAll('div')[0].querySelectorAll('textarea')[0];
-    const APIURL = `${process.env.REACT_APP_BASE_API_URL}/contact`;
+  state = {
+    error:"",
+  }
 
-    if (!textArea.value) return infoAlert({ text: 'Verifica la casilla' });
-    axios.post(`${APIURL}/recapcha`, {
-      response: textArea.value,
-    }).then(({ data }) => {
-      if (data.success) {
-        message.success('Ahora puedes contactarnos.');
-        setDisabledSendButton(false);
-      }
+  onChange = e => this.setState({[e.target.name]: e.target.value})
+
+  sendForm = e => {
+    e.preventDefault()
+    fetch(`${baseURL}/contact`, {
+      body: JSON.stringify(this.state),
+      headers: {"Content-Type": "application/json"},
+      method: 'post'
+    }).then(res => {
+      /*   console.log(this.state)*/
+      res.json()
     })
-    .catch((error) => message.error('Intenta de nuevo.'));
-  };
+        .then(resp => {
+          message.info('¡Hemos recibido tu mensaje! Nos pondremos en contacto contigo.')
+          this.props.history.push('/')
+        })
+  }
 
-  const handleSend = (event) => {
-    const APIURL = `${process.env.REACT_APP_BASE_API_URL}/contact`;
+  render() {
+    return (
+        <section className="app">
+          <NavBar/>
+          <section className="sesion">
+            <div className="cover">
+              <div className="descript">
+                <h1>
+                  CONTACT
+                  <span>O</span>
+                </h1>
+              </div>
+            </div>
+          </section>
+          <section className="form-contacto">
+            <h2 className="titles">¿Qué podemos hacer por ti?</h2>
+            <form onSubmit={this.sendForm}>
+              <TextField
+                  width="100%"
+                  value={this.state.mail}
+                  onChange={this.onChange}
+                  errorMessage={"La dirección de correo no es correcta"}
+                  name="mail"
+                  type="email"
+                  label="¿Cuál es tu correo?"
+                  marginBottom="0px" />
+              <TextField
+                  width="100%"
+                  value={this.state.nombre}
+                  onChange={this.onChange}
+                  name="nombre"
+                  type="text"
+                  label="¿Cuál es tu nombre?"
+                  marginBottom="0px" />
+              <br/>
+              <label style={{color: "#333333", fontWeight: "bold"}} htmlFor="">¿De qué se trata?</label>
+              <br/>
 
-    axios.post(APIURL, contactData)
-      .then(response => console.log(response))
-      .catch(error => console.log(error));
-  };
+              <Select onChange={value=>this.onChange({target:{name:'asunto', value}})}  defaultValue="Tema" style={{width: "100%"}}>
+                <Option value="Academico"  name="academico" >Académico</Option>
+                <Option value="Pagos" name="apagos">Pagos</Option>
+                <Option value="Revista" name="revista" >Revista</Option>
+                <Option value="Técnico" name="tecnico" >Técnico</Option>
+              </Select>
+              <br/>
+              <br/>
+              <button style={{width: "100%"}} className="btn-blue-dark">Enviar</button>
+            </form>
+          </section>
 
-  return (
-    <div className="home-reusables-contact-form">
-      <TextField
-        onChange={handleChange}
-        name="email"
-        label="¿Cuál es tu correo?"
-        value={contactData.email}
-      />
-      <TextField
-        onChange={handleChange}
-        name="name"
-        label="¿Cuál es tu nombre?"
-        value={contactData.name}
-      />
-      <SelectField
-        label="¿De qué se trata?"
-        onChange={value => handleChange({ target: { name: 'topic', value } })}
-        value={contactData.topic}>
-        <OptionSelect value="Académico">Académico</OptionSelect>
-        <OptionSelect value="Pagos">Pagos</OptionSelect>
-        <OptionSelect value="Revista">Revista</OptionSelect>
-        <OptionSelect value="Técnico">Técnico</OptionSelect>
-      </SelectField>
-      <form action="?" method="POST" onSubmit={handleSubmit}>
-        <div class="g-recaptcha" data-sitekey="6LfXl7sUAAAAANAztRRuFtdE19H_FSlduja29Qg-" />
-          <br/>
-        <input type="submit" value="Verificar" style={{ display: 'none'}} />
-      </form>
-      {
-        disabledSendButton && (
-          <Button width="100%" disabled={disabledSendButton} onClick={handleSend}>
-            Enviar
-          </Button>
-        )
-      }
-    </div>
-  );
+        </section>
+    );
+  }
 }
-
 export default HomeContactForm;
